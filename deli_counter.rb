6bb6b7e5b7504657b34e5f416b677321e443c970
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-04 12:20:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,8 @@ def line(array)
 end
 
 def take_a_number(array, name)
-  array << name 
+  array << name
+  puts "Welcome, #{name}. You are number #{array.length} in line."
 end
 
 def now_serving(array)
