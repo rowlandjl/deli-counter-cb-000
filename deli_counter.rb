52279cb64a7c259cb3143141ed4f1b1dcfde78a8
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-04 12:14:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,7 @@ def line(array)
   if array.empty?
     puts "The line is currently empty."
   else
+    current_line = array.map.with_index {|name, index| "#{index + 1}. #{name}"}
     curr_line = array.map.with_index do |name, index|
       "#{index + 1}. #{name}"
     end
